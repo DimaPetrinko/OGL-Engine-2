de52ff46b24fdef913e39701dda7b3fb99945707
Dmytrii Petrinko <petrinko6@gmail.com>
2019-07-24 23:44:28 +0300

    Edits to premake

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,11 @@
 workspace "OGL-Engine-2"
-	location "../"
 	language "C++"
 	cppdialect "C++17"
 	
 	configurations {"Debug", "Release"}
 	platforms { "Win64", "Linux64" }
+
+	startproject = "TestApplication"
 
 	filter {"platforms:Win64"}
 		system "windows"
@@ -19,34 +20,40 @@ workspace "OGL-Engine-2"
 		optimize "On"
 	filter {}
 
-	targetdir ("../build/%{cfg.buildcfg}/%{prj.name}/bin")
-	objdir ("../build/%{cfg.buildcfg}/%{prj.name}/obj")
+	targetdir ("build/%{cfg.buildcfg}/%{prj.name}/bin")
+	objdir ("build/%{cfg.buildcfg}/%{prj.name}/obj")
 
 	project "OGLEngine2"
-		location ("../../%{wks.name}/%{prj.name}")
+		location ("../%{wks.name}/%{prj.name}")
 		kind "StaticLib"
-		files "../%{prj.name}/src/**"
+		files "%{prj.name}/src/**"
 		includedirs 
 		{
-			"../%{prj.name}/src/",
-			"../%{prj.name}/include/"
+			"%{prj.name}/src/",
+			"%{prj.name}/include/"
 		}
-		libdirs {"../%{prj.name}/lib/%{cfg.platform}"}
+		libdirs {"%{prj.name}/lib/%{cfg.platform}"}
 		filter {"platforms:Win64"}
-			defines { "GLEW_STATIC", "Win64" }
+			defines { "GLEW_STATIC", "PLATFORM_WIN64" }
 			links {"glfw3", "glew32s", "opengl32", "Gdi32", "legacy_stdio_definitions"}
 		filter {"platforms:Linux64"}
-			defines { "Linux64" }
+			defines { "PLATFORM_LINUX64" }
 			links {"glfw", "GLEW", "GL"}
 		filter {}
+		--postbuildcommands {"{COPY} copy dll to the TestApplication bin folder"}
 
 	project "TestApplication"
-		location ("../../%{wks.name}/%{prj.name}")
+		location ("../%{wks.name}/%{prj.name}")
 		kind "ConsoleApp"
-		files "../%{prj.name}/src/**"
+		files "%{prj.name}/src/**"
 		includedirs
 		{
-			"../%{prj.name}/src/",
-			"../%{prj.name}/include/"
+			"%{prj.name}/src/",
+			"%{prj.name}/include/"
 		}
+		filter {"platforms:Win64"}
+			defines { "PLATFORM_WIN64" }
+		filter {"platforms:Linux64"}
+			defines { "PLATFORM_LINUX64" }
+		filter {}
 		links {"OGLEngine2"}
